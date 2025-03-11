900f5f7248b783cc4667aa6ab5752cc2ed4b9b75
Chotinat01 <chotinat.s@ku.th>
2025-03-12 01:47:51 +0700

    server

--- user.js
+++ user.js
@@ -4,53 +4,43 @@ window.onload = async () => {
     await loadData();
 }
 
-const loadData = async () => {
-    
-    console.log('user page loaded')
-    //1. load user ทั้งหมด จาก api ที่เตรียมไว้
-    const response = await axios.get(`${BASE_URL}/users`);
-
-    console.log(response.data);
-    
-
-    
-    //2. นำ user ทั้งหมด โหลดกลับเข้าไปใน html (แสดงในรูป html)
-    const userDOM = document.getElementById('user');
-    let htmlData = '<div>'
-
-
-
-    for (let i = 0; i < response.data.length; i++) {
-        let user = response.data[i];
+    const loadData = async () => {
         
-        
-        htmlData += `<div>
-        
-        
-        <table>
-            
-            
-            <td width=5%"> ${user.id} </td>
-            <td width="20%"> ${user.firstname} </td>
-            <td width="20%">${user.lastname}</td>
-            <td width="10%">${user.age}</td>
-            <td width="10%">${user.gender}</td>
-            <td width="10%">
-                <a href = 'index4_px_vh_vw.html?id=${user.id}'>
-                    <button class="buttom">Edit</button>
-                </a>
-            </td>
-            <td width="10%">
-                <button class ="delete" data-id = '${user.id}'>Delete</button>
-            </td>
+        console.log('user page loaded')
+        //1. load user ทั้งหมด จาก api ที่เตรียมไว้
+        const response = await axios.get(`${BASE_URL}/users`);
 
-            
-        </table>
-        </div>`
-    }
+        console.log(response.data);
+        
 
-    htmlData += `</div>`
-    userDOM.innerHTML = htmlData;
+        
+        //2. นำ user ทั้งหมด โหลดกลับเข้าไปใน html (แสดงในรูป html)
+        const userDOM = document.getElementById('user');
+        
+        let htmlData = '<div>'
+       
+        for (let i = 0; i < response.data.length; i++) {
+            let user = response.data[i];
+            htmlData += `<div>
+            <table>
+                <td width=5%")> ${user.id} </td>
+                <td width="20%"> ${user.firstname} </td>
+                <td width="20%">${user.lastname}</td>
+                <td width="10%">${user.age}</td>
+                <td width="10%">${user.gender}</td>
+                <td width="10%">
+                    <a href = 'index4_px_vh_vw.html?id=${user.id}'>
+                        <button class="buttom">Edit</button>
+                    </a>
+                </td>
+                <td width="10%">
+                    <button class ="delete" data-id = '${user.id}'>Delete</button>
+                </td>
+            </table>
+            </div>`
+        }
+        htmlData += `</div>`
+        userDOM.innerHTML = htmlData;
 
 
     //3. ลบ user โดยการกดปุ่ม delete
